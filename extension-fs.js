e5613eb5d01d0c432005d738bb8059de56022356
uriegel <uriegel@hotmail.de>
2019-04-20 13:53:41 +0200

    Callbacks with reject parameter

--- extension-fs.js
+++ extension-fs.js
@@ -2,23 +2,23 @@
 const addon = require('./build/Release/extension-fs')
 
 const getDrives = () => new Promise((res, rej) => {
-    addon.getDrives(r => res(r))    
+    addon.getDrives((e, r) => res(r))    
 })
 
 const getFiles = (path) => new Promise((res, rej) => {
-    addon.getFiles(path, r => res(r))    
+    addon.getFiles(path, (e, r) => res(r))    
 })
 
 const getIcon = (url) => new Promise((res, rej) => {
-    addon.getIcon(url, r => res(r))    
+    addon.getIcon(url, (e, r) => res(r))    
 })
 
 const getFileVersion = (file) => new Promise((res, rej) => {
-    addon.getFileVersion(file, r => res(r))    
+    addon.getFileVersion(file, (e, r) => res(r))    
 })
 
 const getExifDate = (file) => new Promise((res, rej) => {
-    addon.getExifDate(file, r => res(r))    
+    addon.getExifDate(file, (e, r) => res(r))    
 })
 
 module.exports.getDrives = getDrives
