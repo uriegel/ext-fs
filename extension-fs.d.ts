7b540ea20612435c5f313487bc26d702b4d889c8
uriegel <uriegel@hotmail.de>
2019-04-28 17:20:04 +0200

    d.ts updated

--- extension-fs.d.ts
+++ extension-fs.d.ts
@@ -35,4 +35,7 @@ declare module 'extension-fs' {
     function getIcon(ext: string): Promise<Buffer>
     function getFileVersion(file: string): Promise<VersionInfo>
     function getExifDate(file: string): Promise<Date>
+    function open(path: string): any
+    function openAs(path: string): any
+    function showInfo(path: string): any
 }
