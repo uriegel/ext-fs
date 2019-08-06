c1fa9d0c769f8236ea50f4b64645cd7b448a90ef
uriegel <uriegel@hotmail.de>
2019-08-06 19:45:19 +0200

    todo

--- windows/get_conflicts.cpp
+++ windows/get_conflicts.cpp
@@ -9,7 +9,7 @@ void get_conflicts(const std::wstring& sourcePath, const std::wstring& targetPat
     std::vector<std::shared_ptr<Conflict>>& conflicts)
 {
     // TODO: Fang an mit 7 Dateien
-    // TODO: sourcepath + items[i]: ist datei oder verzeichnis    
+    // TODO: sourcepath + items[i]: ist datei oder verzeichnis    GetFileAttribute
     // TODO: exisitert es im targetPath?
     // TODO: wenn ja: ConflicItem füllen: Name (mit Unterpath), Datum s/t, Größe s/t, (bei Exe und dll: Version s/t)
     // TODO: Unterverzeichnisse rekursiv ermitteln
