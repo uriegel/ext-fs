7205e980a6e6ae22e1e50122bc5030a1bff907d5
uriegel <uriegel@hotmail.de>
2019-08-07 17:37:02 +0200

    Determining if target exists, if is directory

--- windows/get_conflicts.cpp
+++ windows/get_conflicts.cpp
@@ -1,16 +1,30 @@
 #include <windows.h>
-#include <windows.h>
 #include <string>
 #include <vector>
 #include "get_conflicts.h"
+#include "utils.h"
 using namespace std;
 
-void get_conflicts(const std::wstring& sourcePath, const std::wstring& targetPath, const std::vector<std::wstring> items, 
-    std::vector<std::shared_ptr<Conflict>>& conflicts)
-{
-    // TODO: Fang an mit 7 Dateien
-    // TODO: sourcepath + items[i]: ist datei oder verzeichnis    GetFileAttribute
-    // TODO: exisitert es im targetPath?
-    // TODO: wenn ja: ConflicItem füllen: Name (mit Unterpath), Datum s/t, Größe s/t, (bei Exe und dll: Version s/t)
-    // TODO: Unterverzeichnisse rekursiv ermitteln
+void get_conflicts(const wstring& sourcePath, const wstring& targetPath, const vector<wstring> items, 
+    vector<shared_ptr<Conflict>>& conflicts) {
+    
+    for (auto i = 0U; i < items.size(); i++) {
+        auto path = combine_path(targetPath, items[i]);
+        auto attr = GetFileAttributesW(path.c_str());
+        if (attr != INVALID_FILE_ATTRIBUTES) {
+            path = combine_path(sourcePath, items[i]);
+            attr = GetFileAttributesW(path.c_str());
+            auto isDir = (attr & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY;
+            if (isDir) {
+                // TODO: (2) Alle Dateien und Ordner ermitteln und diese Funktion rekursiv aufrufen
+            } else {
+                // TODO: (1) wenn ja: ConflicItem füllen: Name (mit Unterpath), Datum s/t, Größe s/t, (bei Exe und dll: Version s/t)        
+            }
+        }
+        else {
+            auto err = GetLastError();
+            if (err != 2)
+                ; // TODO: throw exception
+        }
+    }
 }
