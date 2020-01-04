c000ba058a87e2ff4134f231f207af44d45ec196
uriegel <uriegel@hotmail.de>
2020-01-04 11:14:36 +0100

    unicode test

--- dev/index.js
+++ dev/index.js
@@ -4,7 +4,8 @@ console.log("Test", test)
 
 const runLinux = async () => {
     //const path = "A:/Bilder/2017/Abu Dabbab"
-    const path = "/daten/Bilder/Tina/2018/07"
+    //const path = "/daten/Bilder/Tina/2018/07"
+    const path = "/home/uwe/test🙀"
     let exifFiles = await addon.getFiles(path)
 
     let start = process.hrtime.bigint()
